efe654b8b8bbf84f3079beaecc346dc9dee7f81b
Alison <alisonquato@gmail.com>
2022-06-08 20:08:12 -0400

    start here
    logo, add to script

--- FestivalEureka/Assets/CleanWater.cs
+++ FestivalEureka/Assets/CleanWater.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class CleanWater : MonoBehaviour
 {
-    public GameObject EauPropre;
-    public GameObject EauPollue;
+  public GameObject EauPropre;
+  public GameObject EauPollue;
 
-    void OnCollisionEnter (Collision target) 
+  void OnCollisionEnter (Collision target) 
+  {
+    if( target.gameObject.tag.Equals("bloc") == true )
     {
-      if( target.gameObject.tag.Equals("bloc") == true )
-      {
-        //Écrit le code en dessous de cette ligne
+      //Écrit le code en dessous de cette ligne / Write code below this line
         
-        EauPollue.SetActive(false); 
+
+      if(EauPropre.active == false) 
+      {
+        EauPollue.SetActive(true);
+      }
+      else
+      {
+        EauPollue.SetActive(false);
       }
     }
+  }
 }
 
 
